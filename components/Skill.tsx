293ce78abd00a644a85619f42a57d66c0afc6918
Francisco Alderete <franalderete7@gmail.com>
2022-10-27 22:00:58 -0300

    bug: fix typos

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,6 +1,5 @@
 import { motion } from "framer-motion";
 import React from "react";
-import { urlFor } from "../sanity";
 import { Skill } from "../typings";
 
 type Props = {
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,6 @@ import WorkExperience from "../components/WorkExperience";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import ContactMe from "../components/ContactMe";
-import Link from "next/link";
 import { PageInfo, Experience, Project, Skill, Social } from "../typings";
 import { fetchPageInfo } from "../utils/fetchPageInfo";
 import { fetchExperience } from "../utils/fetchExperiences";
