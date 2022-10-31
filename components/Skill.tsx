c360d17b88de1b35797e72035d2021e9d3afa4e9
Francisco Alderete <franalderete7@gmail.com>
2022-10-31 15:41:03 -0300

    bug: add Image from next

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import React from "react";
 import { urlFor } from "../sanity";
 import { Skill } from "../typings";
+import Image from "next/image";
 
 type Props = {
   skill: Skill;
@@ -11,9 +12,9 @@ type Props = {
 const Skill = ({ skill, directionLeft }: Props) => {
   return (
     <div className="group relative flex cursor-pointer">
-      <motion.img
+      <motion.div
         initial={{
-          x: directionLeft ? -85 : 85,
+          x: directionLeft ? -80 : 80,
           opacity: 0,
         }}
         transition={{
@@ -23,10 +24,16 @@ const Skill = ({ skill, directionLeft }: Props) => {
           opacity: 1,
           x: 0,
         }}
-        alt={skill.title}
-        src="https://cdn.sanity.io/images/bhrnkmlu/production/84b5e112d61f8acb6ee4d75443d51e1c1951c920-280x280.png"
-        className="rounded-full border border-gray-500 object-cover w-20 h-20 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
-      />
+      >
+        <Image
+          className="relative rounded-full border border-gray-500 object-cover w-20 h-20 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+          alt={skill.title}
+          src="https://cdn.sanity.io/images/bhrnkmlu/production/84b5e112d61f8acb6ee4d75443d51e1c1951c920-280x280.png"
+          width={100}
+          height={100}
+        />
+      </motion.div>
+
       <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-20 w-20 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
         <div className="flex items-center justify-center h-full">
           <p className="text-3xl font-bold text-black opacity-100">
