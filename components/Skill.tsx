ece452f3991be86f7d0f0c0b96ab34f4e15b3e51
Francisco Alderete <franalderete7@gmail.com>
2022-10-31 15:51:40 -0300

    bug: fix margins

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -26,7 +26,7 @@ const Skill = ({ skill, directionLeft }: Props) => {
         }}
       >
         <Image
-          className="relative rounded-full border border-gray-500 object-cover w-20 h-20 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+          className="relative rounded-full border border-gray-500 object-cover w-16 h-16 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
           alt={skill.title}
           src="https://cdn.sanity.io/images/bhrnkmlu/production/84b5e112d61f8acb6ee4d75443d51e1c1951c920-280x280.png"
           width={100}
